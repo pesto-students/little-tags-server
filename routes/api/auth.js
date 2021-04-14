a46d9a7dbb14387a24428ab2bfb68c045e7d731e
Aamir Alam <aamiralam1991@gmail.com>
2021-04-14 10:42:07 +0530

    Fix auth routes

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -208,4 +208,17 @@ router.post("/google_login", async (req, res) => {
   }
 });
 
+// @route    GET api/auth
+// @desc     Get user by token
+// @access   Private
+router.get("/", auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select("-password");
+    res.json(user);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
+
 module.exports = router;
