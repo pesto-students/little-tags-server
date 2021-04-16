ec171f8f3d92d2752992fcf8ac3029e7b511ec0c
Aamir Alam <aamiralam1991@gmail.com>
2021-04-16 10:45:26 +0530

    Fix ejs crash

--- app.js
+++ app.js
@@ -4,38 +4,38 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 const connectDB = require("./config/db");
 
-const session = require("express-session");
+// const session = require("express-session");
 
 //Init Middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
 
-app.use(
-  session({
-    secret: "keyboard cat",
-    resave: false,
-    saveUninitialized: true,
-    cookie: { secure: true },
-  })
-);
+// app.use(
+//   session({
+//     secret: "keyboard cat",
+//     resave: false,
+//     saveUninitialized: true,
+//     cookie: { secure: true },
+//   })
+// );
 
-app.set("view engine", "ejs");
+// app.set("view engine", "ejs");
 
-//Connect Database
+// //Connect Database
 connectDB();
 
-app.get("/", (req, res) => {
-  res.render("login");
-});
+// app.get("/", (req, res) => {
+//   res.render("login");
+// });
 
-app.get("/signup", (req, res) => {
-  res.render("signup");
-});
+// app.get("/signup", (req, res) => {
+//   res.render("signup");
+// });
 
-app.get("/home", (req, res) => {
-  res.render("home");
-});
+// app.get("/home", (req, res) => {
+//   res.render("home");
+// });
 
 // routes
 
